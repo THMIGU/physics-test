9a3c06c562ace5d916c06275bf2c2692aee21ed4
Kai Parsons <thmigu.kp@gmail.com>
2026-05-25 16:24:25 -0700

    feat: basic window loading and fps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,62 @@
+mod fps;
+mod physics;
+
+use sdl3::event::Event;
+use std::time::{Duration, Instant};
+
+use crate::fps::FPS;
+
+const TICK_RATE: f64 = 120_f64;
+
+const WINDOW_WIDTH: u32 = 800;
+const WINDOW_HEIGHT: u32 = 600;
+
 fn main() {
-    println!("Hello, world!");
+	let sdl_context = sdl3::init().unwrap();
+	let video_subsystem = sdl_context.video().unwrap();
+
+	let window = video_subsystem
+		.window("physics-test", WINDOW_WIDTH, WINDOW_HEIGHT)
+		.position_centered()
+		.build()
+		.unwrap();
+
+	let mut canvas = window.into_canvas();
+
+	let mut event_pump = sdl_context
+		.event_pump()
+		.unwrap();
+
+	let mut last_frame = Instant::now();
+	let mut accumulator = Duration::new(0, 0);
+	let tick_time = Duration::from_secs_f64(1_f64 / TICK_RATE);
+
+	let mut fps = FPS::new();
+
+	'running: loop {
+		let now = Instant::now();
+		let frame_duration = now.duration_since(last_frame);
+		accumulator += frame_duration;
+		last_frame = now;
+
+		for event in event_pump.poll_iter() {
+			match event {
+				Event::Quit {
+					..
+				} => break 'running,
+				_ => {}
+			}
+		}
+
+		while accumulator >= tick_time {
+			accumulator -= tick_time;
+		}
+
+		let display_fps = fps.fps(frame_duration);
+
+		canvas
+			.window_mut()
+			.set_title(&format!("physics-test | {:.0} FPS", display_fps))
+			.unwrap();
+	}
 }
